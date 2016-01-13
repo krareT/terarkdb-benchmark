4d75b616e587788f78fecb389688cb08471b78d6
panfengfeng <panfengfeng@ncic.ac.cn>
2016-01-13 15:40:06 +0800

    test.sh

--- test.sh
+++ test.sh
@@ -3,6 +3,7 @@ rm -rf data/*
 echo 3 > /proc/sys/vm/drop_caches
 free -m
 ./db_bench_rocksdb --benchmarks=fillrandom,readrandom --value_size=64 --num=50000000 --reads=25000000 --write_buffer_size=67108864 --cache_size=201326592 --bloom_bits=5 --db=data
+du -h data
 echo "####rocksdb benchmark finish"
 free -m
 
@@ -11,6 +12,7 @@ rm -rf data/*
 echo 3 > /proc/sys/vm/drop_caches
 free -m
 ./db_bench_wiredtiger --benchmarks=fillrandom,readrandom --value_size=64 --num=50000000 --reads=25000000 --write_buffer_size=67108864 --cache_size=201326592 --bloom_bits=5 --db=data
+du -h data
 echo "####wiredtiger benchmark finish"
 free -m
 
@@ -20,5 +22,6 @@ echo 3 > /proc/sys/vm/drop_caches
 free -m
 cp dbmeta.json data/ 
 ./db_bench_nark --benchmarks=fillrandom,readrandom --value_size=64 --num=50000000 --reads=25000000 --sync_index=0 --db=data
+du -h data
 echo "####nark benchmark finish"
 free -m
