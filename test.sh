dbba1e8a0a2e9789f44193cc03b6e5b1ff7d534e
panfengfeng <panfengfeng@ncic.ac.cn>
2016-01-13 15:54:03 +0800

    modify test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ rm -rf data/*
 echo 3 > /proc/sys/vm/drop_caches
 cp dbmeta.json data/
 free -m
-./db_bench_rocksdb --benchmarks=fillrandom,readrandom --value_size=64 --num=50000000 --reads=25000000 --write_buffer_size=67108864 --cache_size=201326592 --bloom_bits=5 --db=data
+./db_bench_nark --benchmarks=fillrandom,readrandom --value_size=64 --num=50000000 --reads=25000000 --sync_index=0 --db=data
 total_size=`du -s -b data`
 echo $total_size
 echo "####rocksdb benchmark finish"
