c0b7fcd8c482751385c9f18aba7daf8095caeafb
panfengfeng <panfengfeng@ncic.ac.cn>
2016-03-27 15:56:34 +0800

    modify test script for movies

---
+++ in-memory-bench/read/test.sh
@@ -0,0 +1,9 @@
+#movies
+
+./test_movies_rocksdb.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_rocksdb_256_3
+
+./test_movies_terark_index.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_terark_index_256
+
+./test_movies_wiredtiger_over.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_wiredtiger_no_lsm_3_over
+
+./test_movies_terark_index_old.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_terark_index_256_old
