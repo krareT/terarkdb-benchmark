ceaf5e55835201a9f4af31202b5de7037bf0bd06
Lei Peng <rockeet@163.com>
2016-03-29 22:06:42 +0800

    Use xxxNoLock

--- doc/movies/db_movies_terark_index.cc
+++ doc/movies/db_movies_terark_index.cc
@@ -906,10 +906,10 @@ class Benchmark {
 		  const int k = thread->rand.Next() % FLAGS_num;
 		  fstring key(allkeys_.at(k));
 		  clock_gettime(CLOCK_MONOTONIC, &two);
-		  tab->indexSearchExact(indexId, key, &idvec, ctxr.get());
+		  tab->indexSearchExactNoLock(indexId, key, &idvec, ctxr.get());
 		  clock_gettime(CLOCK_MONOTONIC, &three);
 		  for (auto recId : idvec) {
-			  tab->selectColgroups(recId, colgroups, &cgDataVec, ctxr.get());
+			  tab->selectColgroupsNoLock(recId, colgroups, &cgDataVec, ctxr.get());
 			  //tab->selectOneColumn(recId, 1, &val, ctxr.get());
 		  }
 		  clock_gettime(CLOCK_MONOTONIC, &four);
