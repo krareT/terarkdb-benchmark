5d78f415d498bd5860e7db504d2b15f95c6e0bad
panfengfeng <panfengfeng@ncic.ac.cn>
2016-05-08 20:04:20 +0800

    memory workload benchmark scripts

--- in-memory-bench/read/test.sh
+++ in-memory-bench/read/test.sh
@@ -1,11 +1,9 @@
 #movies
 
-#./test_movies_leveldb.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_leveldb_256_3G
+./test_movies_leveldb.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_leveldb_256_3G
 
-#./test_movies_rocksdb.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_rocksdb_256_3G
+./test_movies_rocksdb.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_rocksdb_256_3G
 
-#./test_movies_wiredtiger_over.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_wiredtiger_no_lsm_3_over
+./test_movies_wiredtiger_over.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_wiredtiger_no_lsm_3_over
 
 ./test_movies_terark_index.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_terark_index_100_3G_new
-
-#./test_movies_terark_index_old.sh > /home/panfengfeng/result/in-memory/movies/readrandom_multi_terark_index_100_old
