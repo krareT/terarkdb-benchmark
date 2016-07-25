4e854a5ec5dabbb480c22780864acfce00c45ffc
panfengfeng <panfengfeng@ncic.ac.cn>
2016-07-25 16:27:56 +0800

    fix some bugs

--- on-disk-bench/write/test_movies_terark_index.sh
+++ on-disk-bench/write/test_movies_terark_index.sh
@@ -1,8 +1,8 @@
-nohup dstat -tcmd -D sdc --output /home/panfengfeng/trace_log/on-disk/movies/fillrandom_mulit_terark_index_100_mem2g_16g 2 > nohup.out &
+nohup dstat -tcm --output terarkdb_wiredtiger_test_500M_3 2 > nohup.out &
 
 file=/datainssd/publicdata/movies/movies.txt
 record_num=7911684
-dirname=/experiment
+dirname=/experiment/terarkdb
 
 rm -rf $dirname/*
 export TMPDIR=$dirname
@@ -12,43 +12,43 @@ echo "####Now, running terarkdb benchmark"
 echo 3 > /proc/sys/vm/drop_caches
 free -m
 date
-export TerarkDB_WrSegCacheSizeMB=100
+export TerarkDB_WrSegCacheSizeMB=500
 ../../db_movies_terark_index --benchmarks=fillrandom --num=$record_num --sync_index=1 --db=$dirname --resource_data=$file
 free -m
 date
 du -s -b $dirname
 echo "####terarkdb benchmark finish"
 
-rm -rf $dirname/*
-export TMPDIR=$dirname
-echo $TMPDIR
-cp ../../terarkschema/dbmeta_movies_index.json $dirname/dbmeta.json
-echo "####Now, running terarkdb benchmark"
-echo 3 > /proc/sys/vm/drop_caches
-free -m
-date
-export TerarkDB_WrSegCacheSizeMB=100
-../../db_movies_terark_index --benchmarks=fillrandom --num=$record_num --sync_index=1 --db=$dirname --resource_data=$file --threads=3
-free -m
-date
-du -s -b $dirname
-echo "####terarkdb benchmark finish"
+#rm -rf $dirname/*
+#export TMPDIR=$dirname
+#echo $TMPDIR
+#cp ../../terarkschema/dbmeta_movies_index.json $dirname/dbmeta.json
+#echo "####Now, running terarkdb benchmark"
+#echo 3 > /proc/sys/vm/drop_caches
+#free -m
+#date
+#export TerarkDB_WrSegCacheSizeMB=100
+#../../db_movies_terark_index --benchmarks=fillrandom --num=$record_num --sync_index=1 --db=$dirname --resource_data=$file --threads=3
+#free -m
+#date
+#du -s -b $dirname
+#echo "####terarkdb benchmark finish"
 
-rm -rf $dirname/*
-export TMPDIR=$dirname
-echo $TMPDIR
-cp ../../terarkschema/dbmeta_movies_index.json $dirname/dbmeta.json
-echo "####Now, running terarkdb benchmark"
-echo 3 > /proc/sys/vm/drop_caches
-free -m
-date
-export TerarkDB_WrSegCacheSizeMB=100
-../../db_movies_terark_index --benchmarks=fillrandom --num=$record_num --sync_index=1 --db=$dirname --resource_data=$file --threads=6
-free -m
-date
-du -s -b $dirname
-echo "####terarkdb benchmark finish"
-
+#rm -rf $dirname/*
+#export TMPDIR=$dirname
+#echo $TMPDIR
+#cp ../../terarkschema/dbmeta_movies_index.json $dirname/dbmeta.json
+#echo "####Now, running terarkdb benchmark"
+#echo 3 > /proc/sys/vm/drop_caches
+#free -m
+#date
+#export TerarkDB_WrSegCacheSizeMB=100
+#../../db_movies_terark_index --benchmarks=fillrandom --num=$record_num --sync_index=1 --db=$dirname --resource_data=$file --threads=6
+#free -m
+#date
+#du -s -b $dirname
+#echo "####terarkdb benchmark finish"
+#
 dstatpid=`ps aux | grep dstat | awk '{if($0 !~ "grep"){print $2}}'`
 for i in $dstatpid
 do
